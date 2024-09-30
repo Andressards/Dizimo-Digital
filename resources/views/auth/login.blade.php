8f372dfd882487b0c0343e21ae76ce15d7668ad5
Andressa <andressarodrigues.est@gmail.com>
2024-09-30 20:15:15 -0300

    alteração da logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,10 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img src="{{ asset('img/logo.png') }}" alt="Logo" class="w-20 h-20">
             </a>
         </x-slot>
+
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="tela-inicio" style="display: flex; justify-content: center; align-items: center;">
-    <img src="/img/logo_dizimo_digital.png" alt="logo-inicial" id="logo-inicial">
+    
 </div>
 
 @endsection
